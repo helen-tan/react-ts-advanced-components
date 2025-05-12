3727c0536ba91a9f5082fe1e50e75cf919ffd826
Helen Tan <helentanmz0909@gmail.com>
2025-05-12 14:41:00 +0800

    feat: use of ComponentPropsWithoutRef to allow Input component props to accept standard props of built-in element <input>

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,11 @@
+import type { ComponentPropsWithoutRef } from "react";
 
 type InputProps = {
     label: string;
     id: string;
-}
+} & ComponentPropsWithoutRef<'input'>; 
+// gives an object containing all the default props of the standard built-in element
+// now this component accepts custom props and the all the standard props of that element
 
 // Wrapper component
 export default function Input(props: InputProps) {
@@ -11,7 +14,7 @@ export default function Input(props: InputProps) {
     return (
         <p>
             <label htmlFor={id}>{ label }</label>
-            <input id={id} type="text" />
+            <input {...props} />
         </p>
     )
 }
