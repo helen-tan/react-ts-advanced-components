67b4f88116a3fb5b7698c7761f133ff4c824c1e4
Helen Tan <helentanmz0909@gmail.com>
2025-05-12 17:02:27 +0800

    feat: Foward a ref from parent(App.tsx) to child(Input.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Input from './components/Input';
 import Button from './components/Button';
 import Container from './components/Container';
+import { useRef } from 'react';
 
 function App() {
+  const textInput = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
+      <Input id="name" label="Your name" type="text" ref={textInput}/>
       <Input id="age" label="Your age" type="number" />
       <p>
         <Button>A Button</Button>
